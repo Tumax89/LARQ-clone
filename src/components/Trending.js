48b76b805b3626b32f4c70c06aa40b53fb70dd79
Tumax89 <58259163+Tumax89@users.noreply.github.com>
2021-04-27 23:06:11 +0800

    second

--- src/components/Trending.js
+++ src/components/Trending.js
@@ -3,8 +3,8 @@ import trendingData from "../data/trendingData";
 
 const Trending = () => {
   return (
-    <div className="flex flex-col h-xl lg:h-xxl  px-4 xl:px-16 pt-16 pb-24 bg-blue-100">
-      <h1 className="flex text-6xl font-semibold text-blue-900 items-center justify-center bg-green-100">
+    <div className="flex flex-col h-xl lg:h-xxl  px-4 xl:px-16 pt-16 pb-24 ">
+      <h1 className="flex text-6xl font-semibold text-blue-900 items-center justify-center ">
         Treding now
       </h1>
       <div className="flex flex items-center justify-center pt-6 pr-8">
@@ -18,8 +18,12 @@ const Trending = () => {
               src={products.image}
               alt={products.name}
             />
-            <div className="bg-green-100 lg:px-12">{products.title}</div>
-            <div className="lg:px-12">{products.name}</div>
+            <div className="lg:px-12 text-blue-400 text-xs  pt-6 sm:text-base sm:pt-2">
+              {products.title}
+            </div>
+            <div className="pt-2 lg:px-12 font-extrabold text-base text-blue-900 sm:text-xl ">
+              {products.name}
+            </div>
           </div>
         ))}
       </div>
